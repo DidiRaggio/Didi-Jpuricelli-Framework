d5d2636dc0c34e365ba7c870071e38b2d99b8662
Didi Raggio <diego_raggio@hotmail.com>
2016-03-14 17:34:43 -0300

    homeimage

--- app/models/home_image.rb
+++ app/models/home_image.rb
@@ -4,7 +4,7 @@ class HomeImage < ActiveRecord::Base
     order('rank asc')
   end
 
-  has_attached_file :image, :styles => { :original => "2000x"}, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :original => "1500x"}, :default_url => "/images/:style/missing.png"
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	:image_file_name
