bb7fc3d01805ebece65346f2b72e8a71f811c975
Didi Raggio <diego_raggio@hotmail.com>
2016-03-14 15:42:54 -0300

    compiled precompiled assests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
+//= require_tree 
+//
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
+//= require bootstrap
 //= require turbolinks
-//= require_tree .
+
