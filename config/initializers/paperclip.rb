d2ea7c53e2a0fb077d2d5291838c3284f7a4e697
Didi Raggio <diego_raggio@hotmail.com>
2016-03-17 02:54:45 -0300

    cloudfront working

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
-Paperclip::Attachment.default_options[:url] = 'static%d.jpuricelli.com'
-Paperclip::Attachment.default_options[:path] = '/files/uploads/:class/:id/:style/:class:style:id.:extension'
+# Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+# Paperclip::Attachment.default_options[:url] = 'asset_host'
+# Paperclip::Attachment.default_options[:path] = '/files/uploads/:class/:id/:style/:class:style:id.:extension'
