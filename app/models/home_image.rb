a263cb5912a703593bd91281b9398d2398bc21f8
Didi Raggio <diego_raggio@hotmail.com>
2016-03-17 02:34:29 -0300

    cloudfront working

--- app/models/home_image.rb
+++ app/models/home_image.rb
@@ -4,7 +4,7 @@ class HomeImage < ActiveRecord::Base
     order('rank asc')
   end
 
-  has_attached_file :image, :styles => { :original => "1500x"}, :default_url => "/images/:style/missing.png",
+  has_attached_file :image, :styles => { :original => "1500x"},
   # :url => ':s3_alias_url',
   # :s3_host_alias => 'static15.jpuricelli.com',
   # :default_url => '',
