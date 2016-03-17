913c304d3bac66cc9d12b7771327599891ab9018
Didi Raggio <diego_raggio@hotmail.com>
2016-03-16 22:27:34 -0300

    s3 alias

--- app/models/home_image.rb
+++ app/models/home_image.rb
@@ -3,13 +3,10 @@ class HomeImage < ActiveRecord::Base
   def self.to_rank
     order('rank asc')
   end
-  def random0_20
-      rand(0 .. 19)
-  end
 
   has_attached_file :image, :styles => { :original => "1500x"}, :default_url => "/images/:style/missing.png",
   :url => ':s3_alias_url',
-  :s3_host_alias => 'static'random0_20'.jpuricelli.com',
+  :s3_host_alias => 'static15.jpuricelli.com +Math.random()',
   :path => "files/uploads/homeimage/image/:class/:id.:style.:extension"
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
