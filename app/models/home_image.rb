44219d92712bc5e24d579ae86624c4ddb19b0d60
Didi Raggio <diego_raggio@hotmail.com>
2016-03-17 00:13:19 -0300

    cloudfront working

--- app/models/home_image.rb
+++ app/models/home_image.rb
@@ -7,7 +7,7 @@ class HomeImage < ActiveRecord::Base
   has_attached_file :image, :styles => { :original => "1500x"}, :default_url => "/images/:style/missing.png",
   # :url => ':s3_alias_url',
   # :s3_host_alias => 'static15.jpuricelli.com',
-  :path => ':url/:class/:id/:style/:class:style:id.:extension'#"images/:class/:style/:id/:style/:style:id.:extension"
+  :path => ':class/:id/:style/:class:style:id.:extension'#"images/:class/:style/:id/:style/:style:id.:extension"
   
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
