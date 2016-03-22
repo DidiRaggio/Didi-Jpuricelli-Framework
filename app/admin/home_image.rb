ec6f9bd8ede17968b752ff99363f957fff122d90
Didi Raggio <diego_raggio@hotmail.com>
2016-03-22 01:07:20 -0300

    cloudfront

--- app/admin/home_image.rb
+++ app/admin/home_image.rb
@@ -19,8 +19,8 @@ ActiveAdmin.register HomeImage do
       	row :logoWhite
         row :rank
         row :image do 
-           homeImage.image.url(:original).sub('/images', '')
-        	image_tag(homeImage.image.url(:original).sub(/images/, ''))
+           homeImage.image.url(:original)
+        	image_tag(homeImage.image.url(:original))
         end
     end
     end
