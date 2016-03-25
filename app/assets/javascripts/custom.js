a54c8c133bab38add858c90ac40447c3d08db1c5
Didi Raggio <diego_raggio@hotmail.com>
2016-03-25 18:25:50 -0300

    fix scroll

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -15,7 +15,7 @@
 		 * Animated scrolling / Scroll Up
 		/* ---------------------------------------------- */
 
-		$('a[href*=#]').bind("click", function(e){
+		$('a[href*=\\#]').bind("click", function(e){
 			var anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $(anchor.attr('href')).offset().top
--- public/assets/application-36bc924aa3af48d0f7f4dd762e46256a06060a349ebe079009cdf48a13aa23ab.js
+++ public/assets/application-36bc924aa3af48d0f7f4dd762e46256a06060a349ebe079009cdf48a13aa23ab.js
@@ -35157,7 +35157,7 @@ if (ischrome) {
 		 * Animated scrolling / Scroll Up
 		/* ---------------------------------------------- */
 
-		$('a[href*=#]').bind("click", function(e){
+		$('a[href*=\\#]').bind("click", function(e){
 			var anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $(anchor.attr('href')).offset().top
