4dd17eef1d7fdbf94d58cae2a167879a68c66e87
Didi Raggio <diego_raggio@hotmail.com>
2016-03-30 15:17:44 -0300

    comment out init

--- public/assets/application-857d23a95cb401c9eabc708b58bd42d39f62453188ac62dc37d6dddf43e79616.js
+++ public/assets/application-857d23a95cb401c9eabc708b58bd42d39f62453188ac62dc37d6dddf43e79616.js
@@ -36588,11 +36588,11 @@ if (typeof jQuery === 'undefined') {
 			type: 'ajax',
 			callbacks: {
 				parseAjax: function(mfpResponse) {
-					$.getScript('d9rke1cfbz5k9.cloudfront.net/assets/js/jquery.fitvids.js', function( data, textStatus, jqxhr ) {
+					$.getScript('https://d9rke1cfbz5k9.cloudfront.net/assets/js/jquery.fitvids.js', function( data, textStatus, jqxhr ) {
   console.log( "got jquery.fitVids"); // Data returned
   
 					});
-					$.getScript('d9rke1cfbz5k9.cloudfront.net/assets/custom-portfolio.js');
+					$.getScript('https://d9rke1cfbz5k9.cloudfront.net/assets/custom-portfolio.js');
 				},
 			}
 		});
@@ -36713,10 +36713,10 @@ if (typeof jQuery === 'undefined') {
 		$imgs = $projectsContainer.find('img'),
 		imgLoad,
 
-	init = function() {
-		imgLoad = new imagesLoaded($imgs.get());
-		imgLoad.on('always', onAllImagesFinished);
-	},
+	// init = function() {
+	// 	imgLoad = new imagesLoaded($imgs.get());
+	// 	imgLoad.on('always', onAllImagesFinished);
+	// },
 
 	onAllImagesFinished = function(instance) {
 
@@ -36731,9 +36731,9 @@ if (typeof jQuery === 'undefined') {
 
 	};
 
-	return {
-		init: init
-	};
+	// return {
+	// 	init: init
+	// };
 
 	}( jQuery, window.imagesLoaded ));
 
