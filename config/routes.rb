bc21d47fa83a6177ba049ea6f6cf6bb8cd80e8b2
Didi Raggio <diego_raggio@hotmail.com>
2016-04-04 20:57:32 -0300

    fix magnificpopup

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/const'
   get '/home', :to => 'pages#home'
-  get '/partialshow', :to => 'galleries/_show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
