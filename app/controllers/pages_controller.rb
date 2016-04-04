d569e16344cdf9638be8f23d55731a143d398e85
Didi Raggio <diego_raggio@hotmail.com>
2016-04-04 18:50:58 -0300

    ajax modal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,4 +14,9 @@ class PagesController < ApplicationController
   	
   end
 
+  private
+  def gallery_params
+      params.require(:gallery).permit( :id, :name, :sub, :assets , :tags, :rank, :menuimage, :description , assets_attributes: [:image, :description, :title, :embeded_video, :assettype_id, :gallery_id], tags_attributes: [:id, :gallery_id, :gallerytype_id])
+  end
+  
 end
