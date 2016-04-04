7bd5f318acaf56608a81cc68ed82ff3b5af913e6
Didi Raggio <diego_raggio@hotmail.com>
2016-04-04 15:56:22 -0300

    get ajax working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
   	@homeImages = HomeImage.all.to_rank
     @galleries = Gallery.all.to_rank
- 	@assets = Asset.all.to_rank
+  	@assets = Asset.all.to_rank
     @gallerytypes = Gallerytype.all 
     @tags = Tag.all
     @abouts = About.all
