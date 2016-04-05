6439ac3d7352aafb855cd0d2e4500a726543f14f
Didi Raggio <diego_raggio@hotmail.com>
2016-04-04 21:37:05 -0300

    magnific popup fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,10 +19,10 @@ class PagesController < ApplicationController
       params.require(:gallery).permit( :id, :name, :sub, :assets , :tags, :rank, :menuimage, :description , assets_attributes: [:image, :description, :title, :embeded_video, :assettype_id, :gallery_id], tags_attributes: [:id, :gallery_id, :gallerytype_id])
   end
 
-#   def render_partial
-#   respond_to do |format|               
-#     format.js
-#     format.json
-#   end        
-# end 
+  def render_partial
+  respond_to do |format|               
+    format.js
+    format.json
+  end        
+end 
 end
