367a152e205e9027afec7cd91b1092d5f1fe4129
Didi Raggio <diego_raggio@hotmail.com>
2016-04-18 03:55:13 -0300

    get contact working

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,11 @@
 class ContactsController < ApplicationController
+	def new
+    @contact = Contact.new
+    end
 	def create
-		@contact = Contact.new(params[:id])
+		@contact = Contact.new(contact_params)
 
-		if @contact.save
+		if @contact.valid?
 
 
 
