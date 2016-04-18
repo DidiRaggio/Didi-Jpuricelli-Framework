39c21cea24704759987eb4e805b94071c4cc9daf
Didi Raggio <diego_raggio@hotmail.com>
2016-04-18 15:22:51 -0300

    fix contact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -35,6 +35,22 @@ class PagesController < ApplicationController
 
   end
 
+    def create
+    @contact = Contact.new(contact_params)
+
+
+    if @contact.valid?
+      ContactMailer.contact_created(@contact).deliver
+      ResponseMailer.response_created(@contact).deliver_now
+      flash[:notice] = "Gracias por contactarse!"
+      # redirect_to :back, :notice => "Gracias por contactarse!"
+    else
+      # redirect_to :back
+      flash[:notice] = "No se pudo contactar :("
+    end
+
+  end
+
   def const
   	
   end
