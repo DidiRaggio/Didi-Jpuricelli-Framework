79126db3793e8c2cf979ad8d68a58cf530ea63af
Didi Raggio <diego_raggio@hotmail.com>
2016-04-18 03:47:50 -0300

    get contact working

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact
   include ActiveModel::Model
-	# attr_accessor :name, :email, :subject, :message
-	# validates :name, :email, :subject, :message, presence: true
+	attr_accessor :name, :email, :subject, :message
+	validates :name, :email, :subject, :message, presence: true
   validates_presence_of :name, presence: true,      :validate => true
   validates_length_of :name, :within => 2..20
   validates_presence_of :email, presence: true,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
