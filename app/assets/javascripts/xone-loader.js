25662e66413a01819984021e5958d332ec7ded82
Didi Raggio <diego_raggio@hotmail.com>
2016-04-20 17:13:11 -0300

    gallery fix

--- app/assets/javascripts/xone-loader.js
+++ app/assets/javascripts/xone-loader.js
@@ -164,7 +164,21 @@ $(document).on('page:load', ready);
 	}
 		
 	function portfolioShow(){
-		jQuery(".owl-carousel").owlCarousel();		
+		jQuery(".owl-carousel").owlCarousel({
+
+  navigation : true, // Show next and prev buttons
+  slideSpeed : 300,
+  paginationSpeed : 400,
+  singleItem:true
+
+   // "singleItem:true" is a shortcut for:
+   // items : 1, 
+   // itemsDesktop : false,
+   // itemsDesktopSmall : false,
+   // itemsTablet: false,
+   // itemsMobile : false
+
+   });		
 		// Show Title
 		jQuery( "#portfolio-single .project-title" ).animate({ 'top': '0', opacity: 1 }, 500, 'easeOutQuart');
 			
