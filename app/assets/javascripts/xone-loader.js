87dbc1f0a041e8a427d959eb84addc14ad79a799
Didi Raggio <diego_raggio@hotmail.com>
2016-04-20 20:53:49 -0300

    gallery fix

--- app/assets/javascripts/xone-loader.js
+++ app/assets/javascripts/xone-loader.js
@@ -180,23 +180,23 @@ $(document).on('page:load', ready);
 		//   singleItem: true
 
 		// });	
-			jQuery(".owl-carousel").owlCarousel({
-				items:1,
-				merge:true,
-				loop:true,
-				margin:10,
-				video:true,
-				lazyLoad:true,
-				center:true,
-				responsive:{
-					480:{
-						items:2
-					},
-					600:{
-						items:4
-					}
-				}
-			});
+    jQuery('.owl-carousel').owlCarousel({
+        items:1,
+        merge:true,
+        loop:true,
+        margin:10,
+        video:true,
+        lazyLoad:true,
+        center:true,
+        responsive:{
+            480:{
+                items:2
+            },
+            600:{
+                items:4
+            }
+        }
+    })
 		// jQuery(".owl-carousel").owlCarousel({
 
 		//      	singleItem: true,
